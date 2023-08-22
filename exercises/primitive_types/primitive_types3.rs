5c39f506b5cbaf54db6f8144c317fb4730880308
danbai <danbai@88.com>
2023-08-22 10:48:47 +0800

    types

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,14 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let mut vec: Vec<i128> = Vec::new();
+    vec.push(1);
+    vec.push(2);
+    vec.push(3);
+
+    let a:&[i128] = &vec;
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
